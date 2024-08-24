974cc3ea81ca6a483f56bcb88492f81ab132f669
sosohax <89521341+sosohax@users.noreply.github.com>
2024-08-24 14:35:03 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,22 +4,32 @@ using namespace std;
 
 int main()
 {
-    double price =0;
-    int quantity=0;
-
-    cout<<"Enter quantity:"<<endl;
-    cin>> quantity;
-    cout<<"Enter Price:"<<endl;
-    cin>> price;
-    //calculate subTotal
-    double subTotal= quantity*price;
-    //Tax@ 15%
-    double Tax= subTotal*0.15;
-    //Calculate Total
-    double Total=subTotal+Tax;
-    //dISPLAY
-    cout<<"The Subtotal is:R"<< fixed << setprecision(2)<<subTotal<<endl
-    cout<<"Tax is:R"<< fixed << setprecision(2)<<Tax<<endl
-    cout<<"And The Total is:R"<< fixed << setprecision(2)<<Total << endl;
+    //2d declaration
+    int salaries[4][5];
+    //size of row and colum
+    int n1= sizeof(salaries)/sizeof(salaries[0]);
+    int n2= sizeof(salaries[0])/sizeof(salaries[0][0]);
+    //get user input
+    for(int departments=0;departments<n1;departments++)
+    {
+        cout<<"Enter salary's for employees in Department "<<departments+1<<endl;
+        for(int employees=0;employees<n2;employees++)
+        {
+            cout<<"Employee "<<employees+1<<": R";
+            cin>>salaries[departments][employees];
+        }
+    }
+    //Calculations
+      cout<<endl;
+    for(int departments=0;departments<n1;departments++)
+    {
+        double sum=0;
+       for(int employees=0;employees<n2;employees++)
+       {
+           sum+=salaries[departments][employees];
+       }
+       double AVG = sum/5;
+       cout<<"Average salary of employees in department "<<departments+1<<": "<<AVG<<endl;
+    }
     return 0;
 }
